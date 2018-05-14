7219fae0e0d0acd2af8258c445244ff495ba84cf
JJ Mubang <jmubang@gmail.com>
2018-05-14 14:40:55 -0400

    made page more dynamic

--- Week3CodeChallenge/bookshelf.js
+++ Week3CodeChallenge/bookshelf.js
@@ -13,10 +13,24 @@ function Book(title, author, genre, cover) {
 
 
 var bookShelf = document.querySelectorAll(".table");
-console.log(bookShelf);
 
 var booksTableBody = document.querySelectorAll("#books")
 var trows = booksTableBody[0].children
+
+var books = [new Book("Gifted Hands", "Dr. Benjamin Carson", "Auto-Biography", "https://images-na.ssl-images-amazon.com/images/I/41cj8NKr68L._SX308_BO1,204,203,200_.jpg"),
+new Book("Influence", "Robert B Cialdini, PH.D", "Self Improvement", "https://images-na.ssl-images-amazon.com/images/I/512-B-1yXuL._SX331_BO1,204,203,200_.jpg"),
+new Book("Thinking Fast and Slow", "Daniel Kahneman", "Self Improvement", "https://upload.wikimedia.org/wikipedia/en/c/c1/Thinking%2C_Fast_and_Slow.jpg")];
+
+books.forEach(function(item, value) {
+	var img = document.createElement("img");
+	var row = booksTableBody[0].insertRow(value);
+	var cell1 = row.insertCell(0).innerHTML= item.title;
+	var cell2 = row.insertCell(1).innerHTML= item.author;
+	var cell3 = row.insertCell(2).innerHTML= item.genre;
+	img.src=item.cover;
+	var cell4 = row.insertCell(3).appendChild(img);
+
+})
 
 // event listener for each row, when clicked redirect to page with specified book
 booksTableBody.forEach(function(book) {
@@ -25,29 +39,33 @@ booksTableBody.forEach(function(book) {
 	})
 })
 
-var books = {
-	table: []
-};
 
 var count = 0;
 
 // on submit save form data to json file
-var form = document.querySelector('form')
-form.addEventListener("submit", function() {
+var form = document.querySelector('#button')
+form.addEventListener("click", function() {
 	var title = document.getElementById("Title").value;
 	var author = document.getElementById("Author").value;
 	var genre = document.getElementById("Genre").value;
 	var cover = document.getElementById("Cover").value;
 
-	var book = new Book(title, author, genre, cover)
-	obj.table.push({id: count++, "title": title, "author":author, "genre":genre,"cover":cover})
+	var img = document.createElement("img");
+	var row = booksTableBody[0].insertRow(books.length - 1);
+	var cell1 = row.insertCell(0).innerHTML= title;
+	var cell2 = row.insertCell(1).innerHTML= author;
+	var cell3 = row.insertCell(2).innerHTML= genre;
+	img.src=cover;
+	var cell4 = row.insertCell(3).appendChild(img);
 
-	var json = JSON.stringify()
+	var book = new Book(title, author, genre, cover)
+	books.push(book);
+
+	// obj.table.push({id: count++, "title": title, "author":author, "genre":genre,"cover":cover})
+	//
+	// var json = JSON.stringify(obj)
 
 	console.log(books);
 
 	// document.createElement("trow")
 })
-
-// console.log(form);
-// console.log(booksTableBody[0]);
