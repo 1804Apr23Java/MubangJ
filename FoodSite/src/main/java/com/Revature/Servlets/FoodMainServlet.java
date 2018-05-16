8d6cd715d900295d004c935c0dca64b4e43d37a7
JJ Mubang <jmubang@gmail.com>
2018-05-16 09:42:48 -0400

    worked on foodmainservlet, did not get it working

--- FoodSite/src/main/java/com/Revature/Servlets/FoodMainServlet.java
+++ FoodSite/src/main/java/com/Revature/Servlets/FoodMainServlet.java
@@ -1,6 +1,8 @@
 package com.Revature.Servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +27,9 @@ public class FoodMainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		RequestDispatcher rd = request.getRequestDispatcher("index.html");
+//		rd.forward(request, response);
 	}
 
 	/**
